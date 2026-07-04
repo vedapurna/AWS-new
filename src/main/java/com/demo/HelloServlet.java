2740a49ca71e1e3afc6bf078efd47e2cae76c260
Your Name <you@example.com>
2026-07-05 00:24:15 +0530

    Fix HelloServlet compilation error

--- src/main/java/com/demo/HelloServlet.java
+++ src/main/java/com/demo/HelloServlet.java
@@ -1,12 +1,15 @@
 package com.demo;
 
+import java.io.IOException;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
 
     @Override
     protected void doGet(HttpServletRequest request,
@@ -15,11 +18,18 @@ public class HelloServlet extends HttpServlet {
 
         response.setContentType("text/html");
 
+        response.getWriter().println("<!DOCTYPE html>");
         response.getWriter().println("<html>");
-        response.getWriter().println("<head><title>Jenkins Demo</title></head>");
+        response.getWriter().println("<head>");
+        response.getWriter().println("<title>Jenkins CI/CD Demo</title>");
+        response.getWriter().println("</head>");
         response.getWriter().println("<body>");
         response.getWriter().println("<h1>Java Web Application</h1>");
-        response.getWriter().println("<h2>Successfully deployed using Jenkins!</h2>");
+        response.getWriter().println("<h2>Successfully Deployed Using Jenkins Pipeline!</h2>");
+        response.getWriter().println("<p>Build Tool : Maven</p>");
+        response.getWriter().println("<p>Version Control : GitHub</p>");
+        response.getWriter().println("<p>CI Tool : Jenkins</p>");
+        response.getWriter().println("<p>Application Server : Apache Tomcat</p>");
         response.getWriter().println("</body>");
         response.getWriter().println("</html>");
     }
