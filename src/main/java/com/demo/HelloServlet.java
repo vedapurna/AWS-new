d59fe36c7d04b19b83dded56159866002059a656
Your Name <you@example.com>
2026-07-05 00:13:59 +0530

    updated file

--- src/main/java/com/demo/HelloServlet.java
+++ src/main/java/com/demo/HelloServlet.java
@@ -1,10 +1,10 @@
 package com.demo;
 
-import java.io.IOException;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
 
